42950830bdb424826db84ba4c1c13a4a3a3a31c0
ssaavedraa <ssaavedraa@santiagosaavedra.com.co>
2023-10-25 07:14:27 -0500

    add plantula font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({
+  weight: '600',
+  preload: true,
+  subsets: ['latin'],
+  fallback: ['Sans-serif']
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="es">
+      <body className={poppins.className}>{children}</body>
     </html>
   )
 }
